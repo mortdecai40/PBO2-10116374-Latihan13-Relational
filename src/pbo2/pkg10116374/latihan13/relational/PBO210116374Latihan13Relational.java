51a538bc708bc3ca2fd9d81ba1b16c40af93cf37
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 14:29:33 +0700

    Program Relational

---
+++ src/pbo2/pkg10116374/latihan13/relational/PBO210116374Latihan13Relational.java
@@ -0,0 +1,24 @@
+package pbo2.pkg10116374.latihan13.relational;
+
+/** @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk melakukan Proses Relational
+ */
+public class PBO210116374Latihan13Relational {
+
+       public static void main(String[] args) {
+           int bil1 = 10;
+           int bil2 = 20;
+           
+           System.out.println("a == b = " + (bil1 == bil2));
+           System.out.println("a != b = " + (bil1 != bil2));
+           System.out.println("a > b = " + (bil1 > bil2));
+           System.out.println("a < b = " + (bil1 < bil2));
+           System.out.println("b >= a = " + (bil2 >= bil1));
+           System.out.println("b <= a =" + (bil2 <= bil1));
+           
+    }
+    
+}
